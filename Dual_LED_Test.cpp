6ab7a0a77d41c442841b5c7f930f7512879e2efb
B-Poirot-KTech <109818937+B-Poirot-KTech@users.noreply.github.com>
2024-04-14 22:15:54 -0400

    Update Dual_LED_Test.cpp

--- Dual_LED_Test.cpp
+++ Dual_LED_Test.cpp
@@ -4,6 +4,8 @@
 #include "hardware/clocks.h"
 #include "debounce.pio.h"
 
+
+//CONSTANTS & GLOBALS
 const uint BUTTON0_SOCD_PIN = 14; //output for SOCD cleaned input a, driving LED during testing
 const uint BUTTON0 = 15; //input a
 const uint PRE_SOCD_LED0 = 5; //used as outputs from PIO sm - debounced button presses
@@ -22,14 +24,19 @@ uint offset;
 float clock_div = (float)clock_get_hz(clk_sys) / pio_freq;
 
 
+//setup for debounce PIO program
 void debounce_pio_setup(bool clock_div_enable = false, float clock_div = 1)
 {
+    //pull buttons high
     gpio_pull_up(BUTTON0);
     gpio_pull_up(BUTTON1);
 
+    //grab state machines from PIO_0
     pio = pio0;
     sm0 = pio_claim_unused_sm(pio, true);
     sm1 = pio_claim_unused_sm(pio, true);
+
+    //load program into pio 
     offset = pio_add_program(pio, &debounce_program);
 
     //initialize state machines
@@ -42,7 +49,7 @@ void debounce_pio_setup(bool clock_div_enable = false, float clock_div = 1)
     return;
 };
 
-
+//processor GPIO setup, for SOCD cleaned outputs
 void gpio_setup()
 {
     gpio_init(BUTTON0_SOCD_PIN);
@@ -54,13 +61,15 @@ void gpio_setup()
     stdio_init_all();
 };
 
-
+//SOCD impleneted: A+B = Neutral
 void socd_clean(uint gpio_a_debounced, uint gpio_b_debounced, uint output_a_gpio, uint output_b_gpio){
 
+    //put SOCD cleaned A on A output, A = (A & !B)
     gpio_put(
         output_a_gpio,
         (gpio_get(gpio_a_debounced) && !(gpio_get(gpio_b_debounced)))
     );
+    //put SOCD cleaned B on B output, B = (A & !B)
     gpio_put(
         output_b_gpio,
         (gpio_get(gpio_b_debounced) && !(gpio_get(gpio_a_debounced)))
@@ -70,17 +79,19 @@ void socd_clean(uint gpio_a_debounced, uint gpio_b_debounced, uint output_a_gpio
 
 
 int main(){
+    //array for holding current SOCD status, for passing to socd_clean(uint . . . )
+    gpio_setup();
+    debounce_pio_setup();
+
     bool pre_socd_input_satuses[2] = {
         gpio_get(PRE_SOCD_GPIOS[0]),
         gpio_get(PRE_SOCD_GPIOS[1])
     };
-    gpio_setup();
-    debounce_pio_setup();
+    
     while (true) {
         pre_socd_input_satuses[0] = gpio_get(PRE_SOCD_GPIOS[0]);
         pre_socd_input_satuses[1] = gpio_get(PRE_SOCD_GPIOS[1]);
         socd_clean(PRE_SOCD_LED0, PRE_SOCD_LED1, BUTTON0_SOCD_PIN, BUTTON1_SOCD_PIN);
-
     };
     return 0;
 };
